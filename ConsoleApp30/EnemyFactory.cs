af5eef3cf138d48bc430b6921f0a55d7c5ab58ee
Israel Vitzman <iv058.l.v@gmail.com>
2025-05-28 12:04:53 +0300

    part3

---
+++ ConsoleApp30/EnemyFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    internal class EnemyFactory : Responcibility
+    {
+
+        static List<Responcibility> responcibilities = new List<Responcibility>();
+        public EnemyFactory(string name, bool ifLife, string life, string voice) 
+            : base (name, ifLife, life, voice)
+        {
+            
+        
+        }
+        public void add (string name, bool ifLife, string life, string voice)
+        {
+            EnemyFactory enemyFactory = new EnemyFactory(name, ifLife, life, voice);    
+            responcibilities.Add(enemyFactory);
+        }
+        
+
+    }
+}
