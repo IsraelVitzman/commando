cc1f1dbbf740823f969cf4495ca17a715ccf10a0
Israel Vitzman <iv058.l.v@gmail.com>
2025-05-28 12:45:01 +0300

    5

---
+++ ConsoleApp30/weponlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    internal class Weponlist :Weapon 
+    {   
+        static List<Weapon> weapons=new List<Weapon>();
+        public Weponlist(string name, string manufacturer, int balls) 
+            : base(name, manufacturer, balls)
+        {
+
+        }
+        public void add(string name, string manufacturer, int balls)
+        {   
+            Weapon weapon = new Weapon(name, manufacturer, balls);
+            weapons.Add(weapon);
+        }
+        
+    }
+}
